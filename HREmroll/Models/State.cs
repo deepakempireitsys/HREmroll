2d4b7bf564cfea45aed5dcd57471dea8be0f599d
manishp-empireitsys <manish.p@empireitsys.com>
2021-06-22 19:43:09 +0530

    Commit details
    Country master
    State  Master
    City  Master
    
    Model-view-controller crud with sp added

---
+++ HREmroll/Models/State.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HREmroll.Models
+{
+    public class State
+    {
+        public int STATE_ID { get; set; }
+
+        [Required(ErrorMessage ="Plese select any country")]
+        [Display(Name ="Country ID")]
+        public int COUNTRY_ID { get; set; }
+        [Display(Name = "State Name")]
+        [Required(ErrorMessage = "Plese enter state name")]
+        public string STATE_NAME { get; set; }
+        [Display(Name = "Description")]
+        [Required(ErrorMessage = "Plese enter description")]
+        public string DESCRIPTION { get; set; }
+        [Display(Name = "Country Name")]
+        public string COUNTRY_NAME { get; set; }
+    }
+}
